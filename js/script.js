c74311d7b6503f09e2ae732f966bbf7fc003068f
albert <example@gmail.com>
2022-08-24 22:49:41 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// alert('Hello');
+// const result = confirm("Are you ready?"); 
+
+// const answer = promt("What is your name", "");=
+//   console.log (answer);
+
+const answers = [];
+
+answers[0] = prompt('Ты че пёс?', '');
+answers[1] = prompt('Как сам?', '');
+answers[2] = prompt('Бухать идём?', '');
+
+document.write(answers);
